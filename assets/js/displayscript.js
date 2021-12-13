cd2e3e63e372283dc12fe7c5ab23785d81962d6a
Raquel <33493284+nsoroma@users.noreply.github.com>
2021-12-13 16:26:11 -0500

    working cards

--- assets/js/displayscript.js
+++ assets/js/displayscript.js
@@ -2,53 +2,52 @@ var TMDB_KEY = config.SECRET_TMDB_KEY;
 
 const BASE_URL = 'https://api.themoviedb.org/3';
 
+//https://api.themoviedb.org/3/search/movie?api_key=2cc4c4492608423940547980d7f61fcf&query=Spider+Man
+
 const API_URL = BASE_URL + '/search/movie?' + TMDB_KEY + '&query=Spider+Man';
 
 const BASE_IMG_URL = 'https://image.tmdb.org/t/p/w500';
 
+
 // *********************************************************************************** //
 //                                         
-const movieCards = document.querySelector(".movie-card");
+const movieCards = document.querySelector("#container");
 
-function createCard([title, release_date, poster_path, overview]) {
+function createCard({title, release_date, poster_path, overview}) {
+    var imageSource = BASE_IMG_URL + poster_path;
+
     let code = `
-    <img src = "${poster_path}" alt="">
     <div class="movie-card">
-    <div class="movie-header">
-        <div class="header-icon-container">
-            <a href="#">
-                <i class="material-icons header-icon"></i>
-            </a>
+        <div class="movie-header" style="background-image: url('${imageSource}');" ></div>
+        <div class="movie-content">
+            <div class="movie-content-header">
+                <a href="#">
+                    <h3 class="movie-title">${title}</h3>
+                </a>
+                <div class="tmdb_logo">
+                </div>
+            </div>
+            <div class="movie-info">
+                <div class="info-section">
+                    <label>Overview</label>
+                    <span>${overview}</span>
+                </div>
+            </div>
+            <div class="movie-info">
+                <div class="info-section">
+                    <label>Release Date</label>
+                    <span>${release_date}</span>
+                </div>
+                
+            </div>
+
         </div>
-    </div><!--movie-header-->
-    <div class="movie-content">
-        <div class="movie-content-header">
-            <a href="#">
-                <h3 class="movie-title">${title}</h3>
-            </a>
-            <div class="mcu-logo"></div>
-        </div>
-        <div class="movie-info">
-            <div class="info-section">
-                <label>Date & Time</label>
-                <span>${release_date}</span>
-            </div><!--date,time-->
-        </div>
-        <div class="movie-info">
-        <div class="info-section">
-            <label>Overview</label>
-            <span>${overview}</span>
-        </div><!--date,time-->
     </div>
-
-    </div><!--movie-content-->
-</div><!--movie-card-->
     `;
+    
     movieCards.innerHTML += code;
-    console.log(movieCards)
+    console.log(movieCards);
 }
-
-//const main = document.getElementById('main');
 
 
 fetchMovies(API_URL);
@@ -56,25 +55,18 @@ fetchMovies(API_URL);
 function fetchMovies(url) {
     fetch(url).then(res => res.json()).then(data => {
         console.log(data.results);
-        createCard(data.results);
- //       displayMovies(data.results);
+
+        data.results.forEach(movieItem => {
+            createCard(movieItem);
+            }); 
+    
     })
 }
 
 
 function displayMovies(data) {
-//    main.innerHTML = '';
-
     data.forEach(movieItem => {
         createCard()
         console.log(release_date);
-        }); 
-//        <img src="${BASE_IMG_URL+ poster_path}" alt="${title}">
-
-    }
-/*
-let movie1 = [
-   "Spider-Man", "2002-05-01", "https://image.tmdb.org/t/p/w500/gSZyYEK5AfZuOFFjnVPUCLvdOD6.jpg", "overview"
-   ]
-   createCard(movie1); */
-    
+    }); 
+}
