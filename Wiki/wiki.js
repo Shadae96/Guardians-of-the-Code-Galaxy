d238e5939ccc0b28b88c7c14e8cef09115c255d3
Shadae96 <84427990+Shadae96@users.noreply.github.com>
2021-12-14 02:37:34 -0500

    Edited code for wiki HTML and JS files
    Edited code for wiki HTML and JS files

---
+++ Wiki/wiki.js
@@ -0,0 +1,68 @@
+const searchUrl = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8[%E2%80%A6]on&origin=*&srlimit=1&srsearch=`;
+const startButton = document.getElementById("button");
+const inputValue = document.querySelector('.js-search-input').value
+let url= searchUrl+inputValue;
+let readMe = document.querySelector(".wiki-article")
+const form = document.querySelector('.js-search-form');
+
+
+// -----------------------------------------------------------------------------------------
+
+startButton.addEventListener("click",goWiki);
+
+console.log(inputValue);
+
+function goWiki(url){
+    fetch(url)
+      .then((res) => res.json()).then((data) => {
+        console.log(data.results);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+    const results= Response.json();
+    return results;
+};
+
+
+
+function displayResults(data) {
+  data.forEach(Article => {
+      displayArticle()
+}); 
+
+
+
+function displayArticle(){}
+
+
+
+
+// function displayResults(results) {
+//     // Get a reference to the `.js-search-results` element
+//     const searchResults = document.querySelector('.js-search-results');
+  
+//     // Iterate over the `search` array. Each nested object in the array can be
+//     // accessed through the `result` parameter
+//     results.query.search.forEach(result => {
+//       const url = `https://en.wikipedia.org/?curid=${result.pageid}`;
+  
+//       // Append the search result to the DOM
+//       searchResults.insertAdjacentHTML(
+//         'beforeend',
+//         `<div class="result-item">
+//           <h3 class="result-title">
+//             <a href="${url}" target="_blank" rel="noopener">${result.title}</a>
+//           </h3>
+//           <a href="${url}" class="result-link" target="_blank" rel="noopener">${url}</a>
+//           <span class="result-snippet">${result.snippet}</span><br>
+//         </div>`
+//       );
+//     });
+//   }
+
+
+//   console.log(url)
+
+};
